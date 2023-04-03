a0b961ce7a79a09180cff01479225b5b6f3a53ad
eun417 <eunahchung0417@gmail.com>
2023-04-03 16:50:33 +0900

    회원목록

--- final_project/src/main/java/kr/co/admin/controller/AdminController.java
+++ final_project/src/main/java/kr/co/admin/controller/AdminController.java
@@ -2,6 +2,7 @@ package kr.co.admin.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.co.admin.model.service.AdminService;
 
@@ -10,5 +11,11 @@ public class AdminController {
 	
 	@Autowired
 	AdminService service;
+	
+	@RequestMapping(value="/memberList.do")
+	public String memberList() {
+		
+		return "admin/memberList";
+	}
 
 }
