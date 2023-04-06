50522aa057ded92b6b141fe39d14b51274d2d742
growgi <growgi@users.noreply.github.com>
2023-04-06 17:06:48 +0900

    daterangepick의 확인 취소 버튼 출력 순서 변경
    취소 확인 → 확인 취소

--- final_project/src/main/webapp/js/daterangepicker.js
+++ final_project/src/main/webapp/js/daterangepicker.js
@@ -111,8 +111,8 @@
                 '</div>' +
                 '<div class="drp-buttons">' +
                     '<span class="drp-selected"></span>' +
-                    '<button class="cancelBtn" type="button"></button>' +
                     '<button class="applyBtn" disabled="disabled" type="button"></button> ' +
+                    '<button class="cancelBtn" type="button"></button>' +
                 '</div>' +
             '</div>';
 
