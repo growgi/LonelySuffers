6ee831648d515baba75e51d86d2ec1194641bbee
Ahn-Noah <eoekdl3@gmail.com>
2023-04-11 17:44:00 +0900

    1대1 채팅 유저부분

---
+++ final_project/src/main/java/kr/co/chat/controller/ChatController.java
@@ -0,0 +1,45 @@
+package kr.co.chat.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
+
+import kr.co.chat.model.service.ChatService;
+import kr.co.chat.model.vo.ChatActive;
+import kr.co.chat.model.vo.ChatContent;
+
+@Controller
+public class ChatController {
+	
+	@Autowired
+	private ChatService service;
+	
+	@ResponseBody
+	@RequestMapping(value = "/chat.do",produces = "application/json;charset=utf-8")
+	public String chat(String memberId) {
+	ChatActive ca = service.selectChatActive(memberId);
+		if(ca == null) {
+			int result = service.insertChatActive(memberId);
+			if(result != 0 ) {
+				return "caOk";
+			}else {
+				return "caNo";
+			}
+		}else {
+			return "caOk";
+		}
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/chatList.do",produces = "application/json;charset=utf-8")
+	public String chatList(String memberId) {
+		ArrayList<ChatContent> list = service.selectChatContent(memberId);
+		return new Gson().toJson(list);
+	}
+}
