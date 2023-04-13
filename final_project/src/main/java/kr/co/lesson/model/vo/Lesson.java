c44387079dba3573f99d8c292e1a3a29d21af1a9
RiboKim <lunatic944@gmail.com>
2023-04-13 18:35:39 +0900

    lessonVO수정

--- final_project/src/main/java/kr/co/lesson/model/vo/Lesson.java
+++ final_project/src/main/java/kr/co/lesson/model/vo/Lesson.java
@@ -15,11 +15,9 @@ import lombok.NoArgsConstructor;
 
 public class Lesson {
 	private int lessonNo;				// Primary key
-<<<<<<< HEAD
+	
 	private int lessonLevel;			// 1 = 초급 / 2 = 중급 / 3 = 상급
-=======
-	private int lessonLevel;		// 강습 분반.       예) 초급자A  초급자B  중급자   숙력자
->>>>>>> Keon's_branch
+
 	private String lessonTitle;			// 상품 웹페이지에 노출될 제목
 	private String lessonCity;			// 광역시/도   부분만. 지역별 필터링에 쓸 것.
 	private int lessonPrice;			// 강습(상품) 가격
@@ -35,7 +33,6 @@ public class Lesson {
 										   -1 = 승인대기중. DB에는 insert됐지만, 고객들에게 노출되지는 않는 상품(게시물)
 	  									   -2 = 승인 반려됨. DB에는 insert됐지만, 고객들에게 노출되지는 않는 상품(게시물)	*/
 	private int lessonScore;			// 별평점후기 Table (RIVEIW)이 변경될때마다, oracle에서 trigger가 작동해서 LESSON 테이블의 LESSON_SCORE를 갱신
-<<<<<<< HEAD
 	private String writer;				// 상품등록자의 MEMBER_ID (글쓴이)
 
 
@@ -43,12 +40,9 @@ public class Lesson {
 	private int lessonTimeLength;		// 종료시간을 얻어내기 위해 받은 강습 소요시간 단위(분)
 	private String lessonStartTimes[];	// 다중 시간대를 한꺼번에 테이블에 여러개의 row로 입력하기 위한 배열
 	private String lessonEndTimes[];
-=======
-	private String Writer;				// 상품등록자의 MEMBER_ID (글쓴이)
 	
 	//level값을 가져오는 것 level1=초급, level2=중급, level3=상급
 	private int level1;
 	private int level2;
 	private int level3;
->>>>>>> Keon's_branch
 }
