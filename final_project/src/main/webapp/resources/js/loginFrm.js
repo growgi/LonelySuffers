df5453ac07fea0dfb72594582a9e46ccf5e43e91
Ahn-Noah <eoekdl3@gmail.com>
2023-04-17 19:14:13 +0900

    중복코드 제거, ID,PW 쿠키저장

--- final_project/src/main/webapp/resources/js/loginFrm.js
+++ final_project/src/main/webapp/resources/js/loginFrm.js
@@ -44,3 +44,77 @@ $('#findPwBtn').on('click',function(){
     $('#findPw').modal();
 })
 
+
+//아이디 비밀번호 정보 쿠키에 저장하는 jQuery
+$(function() {
+    fnInit();
+  });
+  
+  function frm_check() {
+    saveid();
+    savepw();
+  }
+  
+  function fnInit() {
+    var cookieid = getCookie("saveid");
+    var cookiepw = getCookie("savepw");
+    if (cookieid != "") {
+      $("input:checkbox[id='saveId']").prop("checked", true);
+      $('#memberId').val(cookieid);
+    }
+    if (cookiepw != "") {
+      $("input:checkbox[id='savePw']").prop("checked", true);
+      $('#memberPw').val(cookiepw);
+    }
+  }
+  
+  function setCookie(name, value, expiredays) {
+    var todayDate = new Date();
+    todayDate.setTime(todayDate.getTime() + 0);
+    if (todayDate > expiredays) {
+      document.cookie = name + "=" + escape(value) + "; path=/; expires=" + expiredays + ";";
+    } else if (todayDate < expiredays) {
+      todayDate.setDate(todayDate.getDate() + expiredays);
+      document.cookie = name + "=" + escape(value) + "; path=/; expires=" + todayDate.toGMTString() + ";";
+    }
+  }
+  
+  function getCookie(Name) {
+    var search = Name + "=";
+    if (document.cookie.length > 0) { // 쿠키가 설정되어 있다면 
+      offset = document.cookie.indexOf(search);
+      if (offset != -1) { // 쿠키가 존재하면 
+        offset += search.length;
+        // set index of beginning of value
+        end = document.cookie.indexOf(";", offset);
+        // 쿠키 값의 마지막 위치 인덱스 번호 설정 
+        if (end == -1)
+          end = document.cookie.length;
+  
+        return unescape(document.cookie.substring(offset, end));
+      }
+    }
+    return "";
+  }
+  
+  function saveid() {
+    var expdate = new Date();
+    if ($("#saveId").is(":checked")) {
+      expdate.setTime(expdate.getTime() + 1000 * 3600 * 24 * 10); //맨뒤에 10은 10일동안쿠키에 저장한다는 뜻
+      setCookie("saveid", $("#memberId").val(), expdate);
+    } else {
+      expdate.setTime(expdate.getTime() - 1000 * 3600 * 24 * 10);
+      setCookie("saveid", $("#memberId").val(), expdate);
+    }
+  }
+  
+  function savepw() {
+    var expdate = new Date();
+    if ($("#savePw").is(":checked")) {
+      expdate.setTime(expdate.getTime() + 1000 * 3600 * 24 * 10);
+      setCookie("savepw", $("#memberPw").val(), expdate);
+    } else {
+      expdate.setTime(expdate.getTime() - 1000 * 3600 * 24 * 10);
+      setCookie("savepw", $("#memberPw").val(), expdate);
+    }
+  }
